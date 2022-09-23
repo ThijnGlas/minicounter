9546991c90149a084cccd6f4aa98f0a6497d91a8
Thijn <thijnglas@gmail.com>
2022-09-23 15:15:30 +0200

    minicounter af

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+// iporteer React
+import React from "react";
+// importeer React DOM
+import ReactDOM from "react-dom/client";
+// importeer App
+import App from "./App/App";
+// variabele aan die we koppelen aan de root
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// renderen we die variabele 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
